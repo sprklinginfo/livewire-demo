834ae153e485f6e167f808847a4746409c0a6c91
sprklinginfo <jiang.iris@gmail.com>
2022-12-27 22:44:12 -0500

    basic setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Auth\Register;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('/register', Register::class);
